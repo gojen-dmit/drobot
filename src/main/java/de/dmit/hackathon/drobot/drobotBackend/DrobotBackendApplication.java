c69e4a5e6da0b0ccbf250c0e086f6f90af3b1002
Jens Golombek <jens.golombek@bertelsmann.de>
2018-03-09 15:19:53 +0100

    -  Start

---
+++ src/main/java/de/dmit/hackathon/drobot/drobotBackend/DrobotBackendApplication.java
@@ -0,0 +1,12 @@
+package de.dmit.hackathon.drobot.drobotBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DrobotBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DrobotBackendApplication.class, args);
+	}
+}
